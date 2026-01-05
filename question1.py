8589d4b9e4bf650cb652ab02c02fe57eacaa43ea
sona <sonasunnybeena7@gmail.com>
2026-01-05 10:39:41 +0530

    question1 added

---
+++ question1.py
@@ -0,0 +1,13 @@
+# Complete the function printNumber which takes an integer input from the user and prints it on the screen.
+
+class Solution:
+
+    def printnumber(self):
+
+        number = int(input())
+
+        print(number)
+
+solution_instance = Solution()
+
+solution_instance.printnumber()
